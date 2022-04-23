9bbd295fdf47efade28fd15905df5be6ac945492
Konrad Gorski <konrad.gorski@idea.edu.pl>
2022-04-23 13:46:29 +0200

    test: add more button tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -7,4 +7,16 @@ describe("Button", () => {
   test("renders Button component", () => {
     render(<Button>example button</Button>);
   });
+
+  it("styles primary button appropriately", () => {
+    const container = render(<Button variant="primary">Primary</Button>);
+    const button = container.getByRole("button");
+    expect(button).toMatchSnapshot();
+  });
+
+  it("styles secondary button appropriately", () => {
+    const container = render(<Button variant="secondary">Secondary</Button>);
+    const button = container.getByRole("button");
+    expect(button).toMatchSnapshot();
+  });
 });
